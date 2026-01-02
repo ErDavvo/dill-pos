0955191e1dcca902af774bc9abffc33f23709036
ErDavvo <160362047+ErDavvo@users.noreply.github.com>
2026-01-02 10:32:28 +0100

    Bug Fixing 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-  process.env.MONGODB_URI, 
+  process.env.MONGO_URI, 
 );
 
 const DishSchema = new mongoose.Schema({
@@ -34,7 +34,7 @@ app.post('/menu', async (req, res) => {
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
